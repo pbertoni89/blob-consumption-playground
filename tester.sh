0e7ee55fae6083c0121d4835361a127f97e75a51
Patrizio Bertoni <pbertoni@x-next.com>
2022-02-09 15:33:53 +0100

    tester improved

--- tester.sh
+++ tester.sh
@@ -8,14 +8,18 @@ function sigint_handler() {
 }
 trap sigint_handler INT
 
+nCores=$(grep -c ^processor /proc/cpuinfo)
 
 for myElf in pool async; do
 	# shellcheck disable=SC2043
 	for myIms in 34; do
 		for myOms in 100 150; do
-			set -x
-			time cmake-build-release/${myElf} --inms ${myIms} --outms ${myOms} --njobs 4 --debug 0 --nblobs 1000
-			{ set +x ;} 2> /dev/null
+			for myJobs in 1 $((nCores / 2)) $((nCores)); do
+				if [[ ${myJobs} -gt 1 && ${myElf} == "async" ]]; then break; fi
+				set -x
+				time cmake-build-release/${myElf} --inms ${myIms} --outms ${myOms} --njobs ${myJobs} --debug 0 --nblobs 1000
+				{ set +x ;} 2> /dev/null
+			done
 		done
 	done
 done
